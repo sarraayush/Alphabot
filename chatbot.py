541cac7d735210f07f8ac24c09934ed616919b39
Muhammad Inaamullah <69645016+therealinaam@users.noreply.github.com>
2022-12-23 03:41:52 +0500

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,10 +1,62 @@
 import streamlit as st
+import cohere 
 from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
-st.cache(suppress_st_warning=True)
-@st.experimental_singleton
+# initialize the Cohere Client with an API Key
+api_key = st.text_input("Enter your API key", value="", type="password")
+
+co = cohere.Client(api_key=api_key)
+
+embeds = co.embed(texts=list(df['text']),
+                  model='large',
+                  truncate='LEFT').embeddings
+
+search_index = AnnoyIndex(embeds.shape[1], 'angular')
+# Add all the vectors to the search index
+for i in range(len(embeds)):
+    search_index.add_item(i, embeds[i])
+search_index.build(10) # 10 trees
+search_index.save('test.ann')
+
+# Choose an example (we'll retrieve others similar to it)
+example_id = 92
+# Retrieve nearest neighbors
+similar_item_ids = search_index.get_nns_by_item(example_id,10,
+                                                include_distances=True)
+# Format and print the text and distances
+results = pd.DataFrame(data={'texts': df.iloc[similar_item_ids[0]]['text'],
+                             'distance': similar_item_ids[1]}).drop(example_id)
+print(f"Question:'{df.iloc[example_id]['text']}'\nNearest neighbors:")
+
+reducer = umap.UMAP(n_neighbors=20) 
+umap_embeds = reducer.fit_transform(embeds)
+# Prepare the data to plot and interactive visualization
+# using Altair
+df_explore = pd.DataFrame(data={'text': df['text']})
+df_explore['x'] = umap_embeds[:,0]
+df_explore['y'] = umap_embeds[:,1]
+
+# Plot
+chart = alt.Chart(df_explore).mark_circle(size=60).encode(
+    x=#'x',
+    alt.X('x',
+        scale=alt.Scale(zero=False)
+    ),
+    y=
+    alt.Y('y',
+        scale=alt.Scale(zero=False)
+    ),
+    tooltip=['text']
+).properties(
+    width=700,
+    height=400
+)
+chart.interactive()
+                             
+#st.cache(suppress_st_warning=True)
+#@st.experimental_singleton
 
 def execution(input):
     bot = ChatBot("My Bot")
@@ -18,8 +70,9 @@ def execution(input):
     result = bot.get_response(input)
     return st.write(f"Chatbot: {result}")
 
-
+# title of the chatbot 
 st.title("Welcome to Our chatbot")
+
 input = st.text_input('User:')
 print("input here:", input)
 execution(input=input)
